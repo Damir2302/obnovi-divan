14283d71f0eda225f1795c152ca87f2ce0d58e8c
Damir2302 <damir23.02@mail.ru>
2023-05-19 18:22:21 +0300

    fixed

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
 
+    $('#fileUpload').on('change', function() {
+        if ($(this).val().length) {
+          let fileName = $('#fileUpload').val().split("\\");
+          $('#fileUploadLabel span').html(fileName[fileName.length - 1]);
+        }
+    });
+
+    // HEADER FIXED
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop() > $('header').height()) {
+        $('header').addClass('header-fixed')
+      } else if ($(window).scrollTop() == 0) {
+        $('header').removeClass('header-fixed')
+        $('header').removeClass('header-backscroll')
+      }
+    })
+
+    let lastScrollTop = 0;
+    $(window).scroll(function(){
+        let currentScroll = $(this).scrollTop()
+        if (currentScroll > lastScrollTop){
+            $('.header-fixed').removeClass('header-backscroll')
+        } else {
+            $('.header-fixed').addClass('header-backscroll')
+        }
+        lastScrollTop = currentScroll
+    })
 })
